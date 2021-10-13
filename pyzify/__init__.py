c023aaf1f800ad4c0ece8e75730b6e78cc5d7242
mhucka <mhucka@caltech.edu>
2021-10-13 12:59:53 -0700

    Add __init__.py

---
+++ pyzify/__init__.py
@@ -0,0 +1,39 @@
+'''
+__init__.py for Pyzify
+
+Copyright
+---------
+
+Copyright (c) 2021 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+# Package metadata ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# This is set of variables is to identify the Python software package.  These
+# values are _not_ presented in the user interface of DIBS, except for the
+# version number.  The information presented to DIBS users, like the contact
+# email address, are set elsewhere, not here.
+#
+#  ╭────────────────────── Notice ── Notice ── Notice ─────────────────────╮
+#  |    The following values are automatically updated at every release    |
+#  |    by the Makefile. Manual changes to these values will be lost.      |
+#  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
+
+__version__     = '0.0.1'
+__description__ = 'Create self-contained executable Python zipapps using shiv'
+__url__         = 'https://github.com/caltechlibrary/pyzify'
+__author__      = 'Michael Hucka'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause'
+
+
+# Miscellaneous utilities.
+# .............................................................................
+
+def print_version():
+    print(f'{__name__} version {__version__}')
+    print(f'Authors: {__author__}')
+    print(f'URL: {__url__}')
+    print(f'License: {__license__}')
